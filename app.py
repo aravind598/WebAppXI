6497bd5eabe8c2721f35da5763972c9ceeb98303
aravind598 <57538836+aravind598@users.noreply.github.com>
2022-01-21 18:55:38 +0800

    a

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
+from streamlit_tensorboard import st_tensorboard
 import numpy as np
 import time
 import tensorflow as tf
+<<<<<<< Updated upstream
 from PIL import Image
 from img_classifier import prediction, prepare
+=======
+from PIL import Image, ImageOps
+import io
+import requests
+from img_classifier import getOutput, prepare_my, make_my_prediction, make_prediction, createserver, st_capture
+>>>>>>> Stashed changes
 import traceback
 #from img_classifier import our_image_classifier
 # import firebase_bro
@@ -11,8 +19,73 @@ import traceback
 # Just making sure we are not bothered by File Encoding warnings
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
+<<<<<<< Updated upstream
 global models
 #model = tf.keras.models.load_model()
+=======
+global model; global my_model; global tflite_colab; global tflite_model_uint8; global tflite_model 
+
+@st.experimental_singleton
+@cache
+def call_model(model_path):
+    """[Call my model and the EfficientNet model]
+
+    Args:
+        model_path ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    model = tf.keras.models.load_model(model_path)
+    model.make_predict_function()
+    model.summary()
+    return model
+
+@st.experimental_singleton
+@cache
+def call_efficient(model_path):
+    model = tf.keras.models.load_model(model_path)
+    model.make_predict_function()
+    model.summary()
+    return model
+
+@st.experimental_singleton
+@cache
+def call_interpreter(model_path):
+    """[Call tflite interpreter]
+
+    Args:
+        model_path ([type]): [description]
+
+    Returns:
+        [type]: [Returns an interpreter]
+    """
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
+@st.experimental_singleton
+@cache
+def call_uint8(model_path):
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
+
+@st.experimental_singleton
+@cache
+def call_colab(model_path):
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
+
+@st.experimental_memo
+@st.cache
+@cache
+def prepare_my_uint8(bytestr, shape = (1,224,224,3) ):
+    """[prepare the data for uint8 inference]
+>>>>>>> Stashed changes
 
 @st.experimental_singleton # cache the function so predictions aren't always redone (Streamlit refreshes every click)
 def make_prediction(image):
@@ -38,6 +111,7 @@ def main():
     page_icon= ":shark:",
     initial_sidebar_state = "collapsed",
     )
+<<<<<<< Updated upstream
     choose_model = st.sidebar.selectbox(
     "Pick model you'd like to use",
     ("Model 1", # original 10 classes
@@ -45,6 +119,28 @@ def main():
      "Model 3 (11 food classes + non-food class)") # 11 classes (same as above) + not_food class
     )
     menu = ['Home', 'About', 'Contact', 'Feedback']
+=======
+
+    with st.spinner("The magic of our AI is starting.... Please Wait"):
+        model = call_efficient("enetd0")
+        my_model = call_model("mymodel")
+        tflite_model = call_interpreter(model_path="mymodel/model_unquant.tflite")
+        tflite_model_uint8 = call_uint8(model_path="mymodel/model.tflite")
+        tflite_colab = call_colab(model_path="mymodel/my_model.tflite")
+    
+
+        
+    choose_model = st.sidebar.selectbox(
+    "Pick model you'd like to use",
+    ("Model 1 (Custom Model)", # original 10 classes
+     "Model 2 (EfficientNet)", # original 10 classes + donuts
+     "Model 3 (Colab)",
+     "Model 4 (Quantised Model)",
+     "Model 5 (UnQuantised Model)") # 11 classes (same as above) + not_food class
+    )
+    
+    menu = ['Home', 'Stats', 'Contact', 'Feedback']
+>>>>>>> Stashed changes
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
@@ -52,6 +148,28 @@ def main():
         st.title('Title of your Awesome App')
         # Now setting up a header text
         st.subheader("By Your Cool Dev Name")
+<<<<<<< Updated upstream
+=======
+        
+        if st.button("Create Server"):
+            service = st_capture(createserver())
+            if st.button("Delete Server"):
+                pass
+                
+        #sentence = st.text_input('Input your sentence here:')
+        sentence = st.text_input('Input your image url here:') 
+        image = None
+        if sentence:
+            try:
+                response = requests.get(sentence)
+                image = Image.open(io.BytesIO(response.content))
+                image = response.content
+                #st.write(str(sentence))
+            except Exception as e:
+                st.error("Exception occured due to url not having image " + str(e))
+                image = None
+                #st.error()
+>>>>>>> Stashed changes
         # Option to upload an image file with jpg,jpeg or png extensions
         uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
         
@@ -69,11 +187,43 @@ def main():
                 st.image(image,use_column_width=True)
                 st.write("")
                 try:
+<<<<<<< Updated upstream
                     #with st.spinner("The magic of our AI has started...."):
                         #label = our_image_classifier(image)
                     label=make_prediction(image)
                         #time.sleep(8)
                     st.success("We predict this image to be: "+label)
+=======
+                    with st.spinner("The magic of our AI has started...."):
+                            #label = our_image_classifier(image)
+                        if choose_model == "Model 1 (Custom Model)":
+                            #model = call_model()
+                            label=make_my_prediction(my_model,image)
+                            t = time.time() - start
+                        elif choose_model == "Model 2 (EfficientNet)":
+                            #my_model = call_my_model()
+                            label=make_prediction(model,image)
+                            t = time.time() - start
+                            #time.sleep(8)
+                        elif choose_model == "Model 3 (Colab)":
+                            label=getOutput(tflite_colab,prepare_my(image),colab=True)
+                            t = time.time() - start
+                        elif choose_model == "Model 4 (Quantised Model)":
+                            label = getOutput(tflite_model_uint8, input_data)
+                            t = time.time() - start
+                        elif choose_model == "Model 5 (UnQuantised Model)":
+                            #input_data = prepare_my_uint8(image)
+                            label = getOutput(tflite_model, prepare_my(image))
+                            t = time.time() - start
+                        else:
+                            #TODO 
+                            label = "Not yet done"
+                            t = time.time() - start
+                    if placeholder:
+                        placeholder.empty()
+                    st.success("We predict this image to be: "+ label)
+                    st.success("Time Taken "+ str(t))
+>>>>>>> Stashed changes
                     #rating = st.slider("Do you mind rating our service?",1,10)
                 except Exception as e:
                     st.error(e)
@@ -99,10 +249,13 @@ def main():
 
         display_team("Your Awesome Name", "./assets/profile_pic.png","Your Awesome Affliation","hello@youareawesome.com")
 
-    elif choice == "About":
+    elif choice == "Stats":
         # Let's set the title of our About page
-        st.title('About us')
-
+        st.title('Tensorboard Stats of the Run')
+        st.markdown('## Train')
+        st_tensorboard(logdir= "logs/train/", port=5011, width=1080)
+        st.markdown('## Validation')
+        st_tensorboard(logdir= "logs/validation/", port=5011, width=1080)
         # A function to display the company logo
         def display_logo(path):
             company_logo = Image.open(path)
--- img_classifier.py
+++ img_classifier.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
+<<<<<<< Updated upstream
+=======
+from PIL import Image
+import numpy as np
+import io
+from contextlib import contextmanager, redirect_stdout
+from io import StringIO
+>>>>>>> Stashed changes
 
 def prepare(bytestr, img_shape=224, rescale=False, expand_dims=False):
     img = tf.io.decode_image(bytestr, channels=3, dtype=tf.dtypes.float32)
@@ -19,6 +27,133 @@ def prediction(model, pred):
     predarray = tf.keras.applications.efficientnet.decode_predictions(prednumpyarray, top=5)
     return predarray
 
+<<<<<<< Updated upstream
+=======
+
+
+def prediction_my(model, pred):
+    classes = ["Fruit", "Dog", "Person", "Car", "Motorbike", "Flower", "Cat"]
+    # run the inference
+    prediction = model.predict(pred)
+    #print(classes[prediction.argmax()])
+    return classes[prediction.argmax()]
+
+def prepare_my(bytestr: bytes, shape = (1,224,224,3) ):
+    """[summary]
+
+    Args:
+        bytestr (bytes): [image bytestr from read]
+        shape (tuple, optional): [description]. Defaults to (1,224,224,3).
+
+    Returns:
+        ndarray: [Output the data in the form of [1,224,224,3] ]]
+    """    
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape, dtype=np.float32)
+    # Replace this with the path to your image
+    image = Image.open(io.BytesIO(bytestr)).convert('RGB')
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    #size = (img_shape, img_shape)
+    #image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+    return data
+
+
+
+#@st.experimental_singleton # cache the function so predictions aren't always redone (Streamlit refreshes every click)
+def make_prediction(model, image):
+    """
+    Takes an image and uses model (a trained TensorFlow model) to make a
+    prediction. Using the EfficientNet
+
+    Returns:
+     image (preproccessed)
+     pred_class (prediction class from class_names)
+     pred_conf (model confidence)
+    """
+    image_array = prepare(image,expand_dims=True)
+    image_pred = prediction(model,image_array)
+    return str(image_pred)
+
+
+def make_my_prediction(my_model,image):
+    """
+    Takes an image and uses model (a trained TensorFlow model) to make a
+    prediction. Using My Model
+
+    Returns:
+     image (preproccessed)
+     pred_class (prediction class from class_names)
+     pred_conf (model confidence)
+    """
+    #my_model = tf.keras.models.load_model("mymodel")
+    image_array = prepare_my(image)
+    image_pred = prediction_my(my_model,image_array)
+    return str(image_pred)
+
+def prediction_my(model, pred):
+    """[Prediction using my model]
+
+    Args:
+        model ([type]): [description]
+        pred ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    classes = ["Car","Cat","Dog", "Flower", "Fruit", "Motorbike", "Person"]
+    # run the inference
+    prediction = model.predict(pred)
+    #print(classes[prediction.argmax()])
+    return classes[prediction.argmax()]
+
+def getOutput(interpreter, input_data, input_details=None, output_details=None, colab=False):
+    """[Get output from interpreter using tflite models]
+
+    Args:
+        interpreter ([type]): [description]
+        input_data ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    #
+    if not input_details or not output_details:
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+    #print(input_details)
+    #print(output_details)
+    # Test the model on random input data.
+    #input_shape = input_details[0]['shape']
+    #input_data = np.array(np.random.random_sample(input_shape), dtype=np.uint8)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    # The function `get_tensor()` returns a copy of the tensor data.
+    # Use `tensor()` in order to get a pointer to the tensor.
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    
+    if colab:
+        colab_classes = ['Airplane', 'Bird', 'Car', 'Cat', 'Dog', 'Flower', 'Fruit', 'Motorbike', 'Person']
+        return colab_classes[output_data.argmax()]
+    else:
+        classes = ["Car","Cat","Dog", "Flower", "Fruit", "Motorbike", "Person"]
+        return classes[output_data.argmax()]
+    
+    #print(output_data)
+    
+
+
+>>>>>>> Stashed changes
 """
 def our_image_classifier(image):
     '''
@@ -48,3 +183,101 @@ def our_image_classifier(image):
     best_outcome = predictions[0].index(max(predictions[0]))
     print(labels[best_outcome])
     return labels[best_outcome]"""
+
+
+
+"""
+def prepare_my(bytestr, img_shape=224):
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # Replace this with the path to your image
+    image = Image.open(io.BytesIO(bytestr))
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    size = (img_shape, img_shape)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+    return data
+"""
+
+@contextmanager
+def st_capture(output_func):
+    with StringIO() as stdout, redirect_stdout(stdout):
+        old_write = stdout.write
+
+        def new_write(string):
+            ret = old_write(string)
+            output_func(stdout.getvalue())
+            return ret
+        
+        stdout.write = new_write
+        yield
+
+def createserver():
+    from azureml.core import Workspace
+    from azureml.core import Workspace
+    from azureml.core.webservice import AciWebservice, LocalWebservice
+    from azureml.core.webservice import Webservice
+    from azureml.core.model import InferenceConfig, Model
+    from azureml.core.environment import Environment
+    from azureml.core import Workspace
+    from azureml.core.conda_dependencies import CondaDependencies
+    try:
+        ws = Workspace.get( name="azworkspace",
+                            subscription_id='763d1b5b-6b77-4ca2-b25e-c2f3774a3d2a',
+                            resource_group='azureresource')
+
+        myenv = Environment(name="myenv")
+        conda_dependencies = CondaDependencies()
+
+        conda_dependencies.add_conda_package("python==3.9")
+
+        #conda_dependencies.add_conda_package("scikit-learn") 
+        #conda_dependencies.add_conda_package("tensorflow") 
+        #conda_dependencies.add_conda_package("pip") 
+        conda_dependencies.add_pip_package("tensorflow")
+        conda_dependencies.add_pip_package("numpy")
+        conda_dependencies.add_pip_package("pillow") 
+        #conda_dependencies.add_pip_package("azureml-core")
+        #conda_dependencies.add_pip_package("keras")
+        conda_dependencies.add_pip_package("torch")
+        conda_dependencies.add_pip_package("torchvision")
+        conda_dependencies.add_pip_package("torchaudio")
+        conda_dependencies.add_pip_package("timm")
+
+        myenv.python.conda_dependencies = conda_dependencies
+        
+        m = Model.register(#model_path= r"C:\Users\Aravind\OneDrive - Nanyang Technological University\CZ4171\models\mymodel", #model_path= r"C:\Users\Aravind\OneDrive - Nanyang Technological University\CZ4171\models\enetd0"
+                        model_path = "mymodel",
+                        model_name = "mymodel",
+                        description = "Custom Keras Model with 7 classes" , 
+                        workspace = ws)
+            # Create inference configuration based on the environment definition and the entry script
+        #myenv = Environment.from_conda_specification(name="env", file_path=r"C:\Users\Aravind\Downloads\env.yml")
+
+        #myenv.python.conda_dependencies = myenvironment
+
+        inference_config = InferenceConfig(entry_script="entry_script.py", environment=myenv)
+        # Create a local deployment, using port 8890 for the web service endpoint
+        deployment_config = LocalWebservice.deploy_configuration(port=8890)
+        # Deploy the service
+        service = Model.deploy(
+            ws, "ef0", [m], inference_config, deployment_config, overwrite=True, show_output=True)
+        # Wait for the deployment to complete
+        service.wait_for_deployment(True)
+        # Display the port that the web service is available on
+        print(service.port)
+        return service
+    except Exception as e:
+        print(str(e))
+        try:
+            service.delete()
+        except:
+            pass
