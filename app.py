389d3d66739e66414576640509668735a2018b11
aravind598 <57538836+aravind598@users.noreply.github.com>
2022-01-29 10:29:06 +0800

    hjkjk

--- app.py
+++ app.py
@@ -305,11 +305,11 @@ def main():
         if uploaded_file is not None:
             image_bytes = uploaded_file1.read()
             if count > 0:
-                modified_image_byte = cache_image(image_byte=image_bytes, azure=True, camera=True)
+                modified_image_byte = cache_image(image_byte=image_bytes, camera=True)
                 modified_image_byte.seek(0)
                 placeholder = st.image(modified_image_byte, use_column_width=True)
             else:
-                modified_image_byte = cache_image(image_byte=image_bytes, azure=True, camera=False)
+                modified_image_byte = cache_image(image_byte=image_bytes, camera=False)
                 modified_image_byte.seek(0)
                 placeholder = st.image(modified_image_byte, use_column_width=True)
         elif image_bytes is not None:
